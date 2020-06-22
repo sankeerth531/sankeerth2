787246bcac710f140cb307be53a2be0984d07ba3
sankeerth531 <66250297+sankeerth531@users.noreply.github.com>
2020-06-22 21:37:59 +0530

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,73 @@
+var root=document.getElementById('main');
+
+var div1=document.createElement('div');
+div1.classList.add("sub")
+root.appendChild(div1);
+
+ var image=document.createElement('img');
+ image.src="1.jpg";
+ image.classList.add('img');
+ div1.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="sankeerth";
+div1.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="student"
+div1.classList.add("p")
+div1.appendChild(para);
+
+var para1=document.createElement('p');
+para1.textContent="sankeerthreddy53135@gmail.com"
+div1.classList.add("p")
+div1.appendChild(para1);
+
+var para2=document.createElement('p');
+para2.textContent="1452369875"
+div1.classList.add("p")
+div1.appendChild(para2);
+
+var a=document.createElement('a');
+a.href="article.html"
+a.textContent="resume"
+div1.classList.add("a")
+div1.appendChild(a);
+
+
+
+var div2=document.createElement('div');
+div2.classList.add("sub")
+root.appendChild(div2);
+
+ var image=document.createElement('img');
+ image.src="1.jpg";
+ image.classList.add('img');
+ div2.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="sai";
+div2.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="student"
+para.classList.add("p")
+div2.appendChild(para);
+
+var para1=document.createElement('p');
+para1.textContent="gfdhuje@gmail.com"
+div2.classList.add("p")
+div2.appendChild(para1);
+
+var para2=document.createElement('p');
+para2.textContent="13269565984"
+div2.classList.add("p")
+div2.appendChild(para2);
+
+var a=document.createElement('a');
+a.href="article.html"
+a.textContent="resume"
+div2.classList.add("a")
+div2.appendChild(a);
+
+
